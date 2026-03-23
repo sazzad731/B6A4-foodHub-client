0d17a1bb3d3443c882662b42d9af2d906006a664
sazzad731 <sajjadho731@gmail.com>
2026-03-24 05:13:15 +0600

    [CREATED]: Search bar bottom of hero section

--- src/app/(CommonLayout)/page.tsx
+++ src/app/(CommonLayout)/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/modules/home/Hero";
+import SearchBar from "@/components/modules/home/SearchBar";
 
 
 
 export default function Home() {
   return (
     <>
-      <Hero/>
+      <Hero />
+      <SearchBar/>
     </>
   );
 }
